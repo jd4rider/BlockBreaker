6a5437c598143fe092a059defaecc446abc4d6bf
jd4rider <jd4rider@gmail.com>
2019-12-09 00:44:23 -0700

    Midway

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Config Params
+    [SerializeField] Paddle paddle1;
+
+    // state
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
